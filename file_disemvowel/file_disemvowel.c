b9076925aad2c90a65c361a77e603589bef11fde
Nick Plucker <pluck011@morris.umn.edu>
2018-10-04 11:26:10 -0500

    Create buffer to store input in memory to send to copy_non_vowels
    Note: This code doesn't work as intended. Nothing gets outputted to the specified file.

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -2,9 +2,9 @@
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 #define BUF_SIZE 1024
-
 
 bool is_vowel(char letter) {
     letter = tolower(letter);
@@ -41,24 +41,29 @@ int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
 }
 
 void disemvowel(FILE* inputFile, FILE* outputFile) {
-    char* disemvoweledStr;
-    int c;
-    int d;
-    while(1) {
-      int c = fgetc(inputFile);
-      if(feof(inputFile)) {
-	break;
-      }
+    long size;
+
+    fseek(inputFile, 0L, SEEK_END);
+    size = ftell(inputFile);
+    rewind(inputFile);
+
+    char* outbuf = (char*) calloc(size + 1, sizeof(char));
+
+    char* inbuf = (char*) calloc(size + 1, sizeof(char));
+    if (!inbuf) {
+      fclose(inputFile);
+      fputs("memory alloc fails", stderr);
+      exit(1);
     }
 
-    while(1) {
-      int d = fgetc(outputFile);
-      if(feof(outputFile)) {
-	break;
-      }
+    if (1 != fread(inbuf, size, 1, inputFile)){
+      fclose(inputFile);
+      free(inbuf);
+      fputs("entire read fails", stderr);
+      exit(1);
     }
-   
-    disemvoweledStr = copy_non_vowels(strlen(c), c, d);
+    
+    int new_size = copy_non_vowels(strlen(inbuf), inbuf, outbuf);
 }
 
 int main(int argc, char *argv[]) { 
@@ -67,14 +72,6 @@ int main(int argc, char *argv[]) {
 
     inputFile = fopen(argv[1], "r");
     outputFile = fopen(argv[2], "w");
-
-    while(1) {
-      int c = fgetc(inputFile);
-      if(feof(inputFile)) {
-	break;
-      }
-      
-    }
 
     disemvowel(inputFile, outputFile);
 
